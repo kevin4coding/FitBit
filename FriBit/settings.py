0fcbd96e89970904282e1dd750f95706cdb9c6a2
Jiahao Liu <jobsrefer2020@gmail.com>
2021-01-07 23:59:13 -0800

    adding room

--- FriBit/settings.py
+++ FriBit/settings.py
@@ -25,7 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'chat'
+    'chat',
+    'channel',
 ]
 
 MIDDLEWARE = [
@@ -107,3 +108,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# channels
+AGSI_APPLICATION = 'FriBit.asgi.application'
